7ffc2ac98cef5f1bf318d0dc3bbe949adf991499
Juhee-Jang <67873515+Juhee-Jang@users.noreply.github.com>
2022-12-26 22:02:30 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by KIKI on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
